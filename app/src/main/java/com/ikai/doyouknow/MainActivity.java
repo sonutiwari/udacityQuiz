fe6e6052ffbca6d8929b7d7e215f5d3986b27ca4
sonutiwari <ankorha@gmail.com>
2018-06-05 12:20:18 +0530

    finished version

--- app/src/main/java/com/ikai/doyouknow/MainActivity.java
+++ app/src/main/java/com/ikai/doyouknow/MainActivity.java
@@ -1,7 +1,6 @@
 package com.ikai.doyouknow;
 
 import android.content.Context;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -17,19 +16,19 @@ public class MainActivity extends AppCompatActivity {
     // Declaring global objects.
     final int Q1_ANSWER = R.id.question_1_second_logo_radio;
     final int Q2_ANSWER = R.id.question_2_first_logo_radio;
-    final String Q4_ANSWER = "nike";
+    final String Q4_ANSWER = "flipkart";
     final int Q5_ANSWER = R.id.question_5_third_radio;
-    final String Q6_ANSWER = "blue";
+    final String Q6_ANSWER = "white";
     final String Q7_ANSWER = "udacity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Code to set custom text font style from assets folder.
-        Typeface typeface = Typeface.createFromAsset(getAssets(),
-                "fonts/Tangerine-Bold.ttf");
+//
+//        // Code to set custom text font style from assets folder.
+//        Typeface typeface = Typeface.createFromAsset(getAssets(),
+//                "fonts/Tangerine-Bold.ttf");
     }
 
     public void checkQuiz(View v) {
@@ -37,43 +36,43 @@ public class MainActivity extends AppCompatActivity {
 
         int numberOfQuestionsCorrect = 0;
 
-        if( checkQuestion1() ){
+        if (checkAnswer1()) {
             numberOfQuestionsCorrect++;
         } else {
             incorrectAnswersList.add("Question 1");
         }
 
-        if( checkQuestion2() ){
+        if (checkAnswer2()) {
             numberOfQuestionsCorrect++;
         } else {
             incorrectAnswersList.add("Question 2");
         }
 
-        if( checkQuestion3() ){
+        if (checkAnswer3()) {
             numberOfQuestionsCorrect++;
         } else {
             incorrectAnswersList.add("Question 3");
         }
 
-        if( checkQuestion4() ){
+        if (checkAnswer4()) {
             numberOfQuestionsCorrect++;
         } else {
             incorrectAnswersList.add("Question 4");
         }
 
-        if( checkQuestion5() ){
+        if (checkAnswer5()) {
             numberOfQuestionsCorrect++;
         } else {
             incorrectAnswersList.add("Question 5");
         }
 
-        if( checkQuestion6() ){
+        if (checkAnswer6()) {
             numberOfQuestionsCorrect++;
         } else {
             incorrectAnswersList.add("Question 6");
         }
 
-        if( checkQuestion7() ){
+        if (checkAnswer7()) {
             numberOfQuestionsCorrect++;
         } else {
             incorrectAnswersList.add("Question 7");
@@ -89,26 +88,26 @@ public class MainActivity extends AppCompatActivity {
         Context context = getApplicationContext();
         CharSequence text = "You got " + numberOfQuestionsCorrect +
                 "/7 answers right.\n\nRecheck the following:\n" + sb.toString();
-        int duration = Toast.LENGTH_SHORT;
+        int duration = Toast.LENGTH_LONG;
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
     }
 
-    private boolean checkQuestion1() {
-        RadioGroup rg = findViewById(R.id.question_1_radio_group);
+    private boolean checkAnswer1() {
+        RadioGroup question1RadioGroup = findViewById(R.id.question_1_radio_group);
 
-        return rg.getCheckedRadioButtonId() == Q1_ANSWER;
-
-    }
-
-    private boolean checkQuestion2() {
-        RadioGroup rg = findViewById(R.id.question_2_radio_group);
-
-        return rg.getCheckedRadioButtonId() == Q2_ANSWER;
+        return question1RadioGroup.getCheckedRadioButtonId() == Q1_ANSWER;
 
     }
 
-    private boolean checkQuestion3() {
+    private boolean checkAnswer2() {
+        RadioGroup question2RadioGroup = findViewById(R.id.question_2_radio_group);
+
+        return question2RadioGroup.getCheckedRadioButtonId() == Q2_ANSWER;
+
+    }
+
+    private boolean checkAnswer3() {
         CheckBox c1 = findViewById(R.id.question_3_first_checkbox);
         CheckBox c2 = findViewById(R.id.question_3_second_checkbox);
         CheckBox c3 = findViewById(R.id.question_3_third_checkbox);
@@ -117,26 +116,26 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private boolean checkQuestion4() {
+    private boolean checkAnswer4() {
         EditText et = findViewById(R.id.question_4_edit_text);
 
         return et.getText().toString().equalsIgnoreCase(Q4_ANSWER);
     }
 
-    private boolean checkQuestion5() {
+    private boolean checkAnswer5() {
         RadioGroup rg = findViewById(R.id.question_5_radio_group);
 
         return rg.getCheckedRadioButtonId() == Q5_ANSWER;
 
     }
 
-    private boolean checkQuestion6() {
+    private boolean checkAnswer6() {
         EditText et = findViewById(R.id.question_6_edit_text);
 
         return et.getText().toString().equalsIgnoreCase(Q6_ANSWER);
     }
 
-    private boolean checkQuestion7() {
+    private boolean checkAnswer7() {
         EditText et = findViewById(R.id.question_7_edit_text);
 
         return et.getText().toString().equalsIgnoreCase(Q7_ANSWER);
